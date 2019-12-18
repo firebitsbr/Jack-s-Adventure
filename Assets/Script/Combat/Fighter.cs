5cc1837b54f3f3f81f95d72dbffedb7687310efb
Jack Yang <jackyangzzh@gmail.com>
2019-12-19 06:20:50 +0800

    raycast all

---
+++ Assets/Script/Combat/Fighter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attact(Target target)
+        {
+            print("Attacked!");
+        }
+    }
+}
--- Assets/Script/Control/PlayerController.cs
+++ Assets/Script/Control/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control
 {
@@ -11,23 +13,50 @@ namespace RPG.Control
         // Update is called once per frame
         void Update()
         {
+            combatMovement();
+            mouseClickMovement();
+
+        }
+
+        private void combatMovement()
+        {
+            RaycastHit[] hitResult = Physics.RaycastAll(getMouseRay());
+            foreach (RaycastHit item in hitResult)
+            {
+                Target targ = item.transform.GetComponent<Target>();
+                if (targ == null)
+                {
+                    continue;
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    GetComponent<Fighter>().Attact(targ);
+                }
+            }
+        }
+
+        private void mouseClickMovement()
+        {
             if (Input.GetMouseButton(1))
             {
                 moveOnClick();
             }
-
         }
 
         private void moveOnClick()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(getMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().moveTo(hit.point);
             }
+        }
+
+        private static Ray getMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
