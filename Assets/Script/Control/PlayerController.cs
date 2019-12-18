8c14c8feb834e706ac324458fd90a156fae4fed4
Jack Yang <jackyangzzh@gmail.com>
2019-12-19 05:38:26 +0800

    namespace

---
+++ Assets/Script/Control/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                moveOnClick();
+            }
+
+        }
+
+        private void moveOnClick()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().moveTo(hit.point);
+            }
+        }
+    }
+}
--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-
-    // Update is called once per frame
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        if(Input.GetMouseButton(1)){
-            moveOnClick();
-        }
-        updateAnimator();
-    }
 
-    private void moveOnClick(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if(hasHit){
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
-    }
+        // Update is called once per frame
+        void Update()
+        {
 
-    private void updateAnimator(){
-        Vector3 vel = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVel = transform.InverseTransformDirection(vel);
-        float speed = localVel.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+            updateAnimator();
+        }
+
+
+
+        public void moveTo(Vector3 dest)
+        {
+            GetComponent<NavMeshAgent>().destination = dest;
+        }
+
+        private void updateAnimator()
+        {
+            Vector3 vel = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVel = transform.InverseTransformDirection(vel);
+            float speed = localVel.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
