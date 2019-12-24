a9d215ac3dd5c503c23a9cd339c1b12719262948
Jack Yang <jackyangzzh@gmail.com>
2019-12-24 21:28:35 +0800

    fader

--- Assets/Script/Scene Management/Portal.cs
+++ Assets/Script/Scene Management/Portal.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+
+        enum Destiniation
+        {
+            A, B, C
+        }
+
         [SerializeField] int nextScene = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] Destiniation dest;
+
 
         private void OnTriggerEnter(Collider other)
         {
@@ -19,10 +29,41 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (nextScene < 0)
+            {
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(nextScene);
-            print("Scene loaded");
+
+            updatePlayer(getPortal());
+
             Destroy(gameObject);
+        }
+
+        private Portal getPortal()
+        {
+            foreach (Portal i in FindObjectsOfType<Portal>())
+            {
+                if (i == this)
+                {
+                    continue;
+                }
+                if (i.dest != dest)
+                {
+                    continue;
+                }
+                return i;
+            }
+            return null;
+        }
+
+        private void updatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(portal.spawnPoint.position);
+            player.transform.rotation = portal.spawnPoint.rotation;
         }
     }
 }
