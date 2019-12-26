70c242817ab741bd2fe027636651d10db86854a7
Jack Yang <jackyangzzh@gmail.com>
2019-12-26 08:24:12 +0800

    new enemies

--- Assets/Script/Core/Health.cs
+++ Assets/Script/Core/Health.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float health = 100f;
         bool isDead = false;
+
+
 
         public bool IsDead()
         {
             return isDead;
         }
 
+
+
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0);
             if (health <= 0 && !isDead)
+            {
+                GetComponent<Animator>().SetTrigger("die");
+                isDead = true;
+                GetComponent<ActionSchedular>().CancelCurrentAction();
+            }
+        }
+
+        public object CaptureState()
+        {
+            return health;
+        }
+
+        public void RestoreState(object state)
+        {
+            health = (float)state;
+
+            if (health == 0)
             {
                 GetComponent<Animator>().SetTrigger("die");
                 isDead = true;
